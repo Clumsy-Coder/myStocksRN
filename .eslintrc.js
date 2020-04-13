2217b038b34fac9ecb95e0465e60d285c90a558f
Clumsy-Coder <ubari94@gmail.com>
2020-04-13 10:01:29 -0600

    Update eslintrc
    - Define linting rules
    - Define parserOptions rules
    - Define eslint parser
    - Define plugins to use
    - Define custom linting rules
    - Handle React native relative path import hell
      - Instead of using relative paths, use the imports like importing npm packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'jest'],
+  rules: {
+    'react/jsx-one-expression-per-line': ['off'],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreUrls: true,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+  ignorePatterns: ['test-results'],
 };
