6642d82a9d380fd55e45d2beaa50924681668727
Clumsy-Coder <ubari94@gmail.com>
2020-04-19 09:45:48 -0600

    Add react-native-dotenv npm package
    The project will use .env file to store build version and API_KEY
    
    NOTE: typescript module for react-native-dotenv has to be created. Placed in src/config/env.d.ts
    This module ensure Typescript that the environment variable in .env do exist and cause no issue
    
    check https://github.com/zetachang/react-native-dotenv/issues/76#issuecomment-585171009

---
+++ src/configs/env.d.ts
@@ -0,0 +1,6 @@
+// Create module to deal with Typescript error when pulling env values using react-native-dotenv
+// check https://github.com/zetachang/react-native-dotenv/issues/76#issuecomment-585171009
+declare module 'react-native-dotenv' {
+  export const BUILD_VERSION: 'development' | string;
+  export const API_KEY: string;
+}
