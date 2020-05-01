7cc84d3def9e168930df3bc439448d23e330b7d6
Clumsy-Coder <ubari94@gmail.com>
2020-04-30 22:23:03 -0600

    Connect redux devtools with the Redux store
    This will allow React native debugger to recognize the devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import logger from 'redux-logger';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { BUILD_VERSION } from 'react-native-dotenv';
 import rootReducer from 'src/redux/index.reducers';
@@ -11,6 +13,6 @@ if (BUILD_VERSION === 'development') {
   middleware.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
