7b26bec65f1b0d400a500853ae37d06f7ce871c1
Clumsy-Coder <ubari94@gmail.com>
2020-05-01 18:47:24 -0600

    Set react navigation routes for Drawer and stack
    Currently the app will have on Drawer navigator and two Stack navigator
    
    Drawer navigator will be used to move between different sections (such as settings)
    
    Stack navigators:
    - HomeStackNavigator: contains stack navigator that displays the stock
      quote and the route to display the stock details screen.
      - These are the only two screens in this stack to be used at the moment
    - SettingStackNavigator: contains all the settings for the entire app
      (such as using dark mode)
      - Also has a route to the About page (contains a description of the
        app), also will contain the build version (ex: 1.0) and build ID (commit ID)
    
    The NavigationContainer is where all the routes are placed to.
    It only accepts ONE navigator type (so I went with Drawer navigator)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-// import Home from 'src/screens/Home';
-// import StockDetails from 'src/screens/StockDetails';
-// import Settings from 'src/screens/Settings';
-// import About from 'src/screens/About';
+import NavigationRoutePath from 'src/routes/RoutePath.enum';
+
+import HomeScreen from 'src/screens/Home';
+import StockDetailsScreen from 'src/screens/StockDetails';
+import SettingsScreen from 'src/screens/Settings';
+import AboutScreen from 'src/screens/About';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-export const StackNavigator = () => (
-  <Stack.Navigator>
-    {/* <Stack.Screen name='Home' component={Home} /> */}
-    {/* <Stack.Screen name='Stock details' component={StockDetails} /> */}
-    {/* <Stack.Screen name='About' component={About} /> */}
+export const HomeStackNavigator = () => (
+  <Stack.Navigator initialRouteName={NavigationRoutePath.Home}>
+    <Stack.Screen name={NavigationRoutePath.Home} component={HomeScreen} />
+    <Stack.Screen name={NavigationRoutePath.StockDetails} component={StockDetailsScreen} />
   </Stack.Navigator>
 );
 
+export const SettingStackNavigator = () => (
+  <Stack.Navigator initialRouteName={NavigationRoutePath.Settings}>
+    <Stack.Screen name={NavigationRoutePath.Settings} component={SettingsScreen} />
+    <Stack.Screen name={NavigationRoutePath.About} component={AboutScreen} />
+  </Stack.Navigator>
+);
+
+// where all the screens come together
+// NOTE: react-navigator only allows ONE navigator type as the child of NavigationContainer
 export const DrawerNavigator = () => (
-  <Drawer.Navigator>
-    {/* <Drawer.Screen name='Home' component={Home} /> */}
-    {/* <Drawer.Screen name='Settings' component={Settings} /> */}
+  <Drawer.Navigator initialRouteName={NavigationRoutePath.Home}>
+    <Drawer.Screen name={NavigationRoutePath.Home} component={HomeStackNavigator} />
+    <Drawer.Screen name={NavigationRoutePath.Settings} component={SettingStackNavigator} />
   </Drawer.Navigator>
 );
