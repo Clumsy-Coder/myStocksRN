5bbbec852cd0715068c9c0e88d451d91d476fed9
Clumsy-Coder <ubari94@gmail.com>
2020-05-06 18:58:47 -0600

    Add type definitions when using navigation as a prop in all the screens
    This is to satisfy typescript error checking

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 import NavigationRoutePath from 'src/routes/RoutePath.enum';
+import { RootStackParamList } from 'src/routes/AppRouteTypes';
 
-const Home = ({ navigation }) => (
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
+};
+
+const Home = ({ navigation }: Props) => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Text>Home screen</Text>
     <Button
--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 import NavigationRoutePath from 'src/routes/RoutePath.enum';
+import { RootStackParamList } from 'src/routes/AppRouteTypes';
 
-const SettingScreen = ({ navigation }) => {
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'Settings'>;
+};
+
+const SettingScreen = ({ navigation }: Props) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Setting screen</Text>
