793dceabe694e450e623617168f61c3ceaf59a4a
Clumsy-Coder <ubari94@gmail.com>
2020-05-11 09:13:13 -0600

    Add tests to be run in directories other than __tests__ folder
    The test files will grouped with their respective source files
    
    ex: redux/Stocks will have a tests/ folder that contains tests regarding
    tests for Stocks actions, actionTypes, reducer, sagas and selectors

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', 'module:react-native-dotenv'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    'module:react-native-dotenv',
+    '@babel/preset-env',
+  ],
   plugins: [
     [
       'module-resolver',
