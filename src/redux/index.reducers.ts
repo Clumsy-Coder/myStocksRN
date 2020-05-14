aa1fda789bafbccc8512d9452ae6d1d4e1c37621
Clumsy-Coder <ubari94@gmail.com>
2020-05-14 11:26:28 -0600

    Add RootActions type for redux actions

--- src/redux/index.reducers.ts
+++ src/redux/index.reducers.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 
 import StockReducer from 'src/redux/Stocks/Reducer';
+import { StockActionTypes } from 'src/redux/Stocks/Types';
 
 const rootReducer = combineReducers({
   Stocks: StockReducer,
@@ -8,3 +9,4 @@ const rootReducer = combineReducers({
 
 export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>;
+export type RootActions = StockActionTypes;
