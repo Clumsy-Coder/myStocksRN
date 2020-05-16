7ab1d7d03aec1d7283aead367a4636548f9d500f
Clumsy-Coder <ubari94@gmail.com>
2020-05-16 08:37:34 -0600

    Update jest config to support async-storage and nativebase
    Async-storage will be mocked during tests
    
    nativebase components will be ignored during tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,9 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 // eslint-disable-next-line global-require
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
