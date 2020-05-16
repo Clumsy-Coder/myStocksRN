0fd9a35fe0017910d357064cdafc30bece380cbc
Clumsy-Coder <ubari94@gmail.com>
2020-05-16 09:49:00 -0600

    Add tests for Favorites reducer, actions and selectors

---
+++ src/redux/Favorites/tests/Selectors.test.ts
@@ -0,0 +1,77 @@
+import * as selectors from 'src/redux/Favorites/Selectors';
+import { RootState } from 'src/redux/index.reducers';
+
+describe('Favorites selectors', () => {
+  describe('Empty store', () => {
+    it('Selects Favorites with empty reducer', () => {
+      const store: RootState = {
+        Stocks: {},
+        Favorites: {
+          symbols: [],
+        },
+      };
+      const expected = {
+        symbols: [],
+      };
+
+      expect(selectors.selectFavorites(store)).toEqual(expected);
+    });
+
+    it('Selects Favorite symbols with empty store', () => {
+      const store: RootState = {
+        Stocks: {},
+        Favorites: {
+          symbols: [],
+        },
+      };
+      const expected: string[] = [];
+
+      expect(selectors.selectFavoriteSymbols(store)).toEqual(expected);
+    });
+  });
+
+  describe('Stored favorite stock symbols', () => {
+    it('Selects Favorite reducer state', () => {
+      const store: RootState = {
+        Stocks: {},
+        Favorites: {
+          symbols: ['AAPL', 'AMZN', 'SHOP'],
+        },
+      };
+      const expected = {
+        symbols: ['AAPL', 'AMZN', 'SHOP'],
+      };
+
+      const actual = selectors.selectFavorites(store);
+      expect(actual).toEqual(expected);
+      expect(actual).toHaveProperty('symbols');
+      expect(actual.symbols).toHaveLength(3);
+      expect(typeof actual.symbols[0]).toBe('string');
+      expect(typeof actual.symbols[1]).toBe('string');
+      expect(typeof actual.symbols[2]).toBe('string');
+      expect(actual.symbols[0]).toBe('AAPL');
+      expect(actual.symbols[1]).toBe('AMZN');
+      expect(actual.symbols[2]).toBe('SHOP');
+    });
+
+    it('Selects Favorite symbols', () => {
+      const store: RootState = {
+        Stocks: {},
+        Favorites: {
+          symbols: ['AAPL', 'AMZN', 'SHOP'],
+        },
+      };
+      const expected = ['AAPL', 'AMZN', 'SHOP'];
+
+      const actual = selectors.selectFavoriteSymbols(store);
+      expect(actual).toEqual(expected);
+      expect(actual).toHaveLength(3);
+      expect(typeof actual[0]).toBe('string');
+      expect(typeof actual[1]).toBe('string');
+      expect(typeof actual[2]).toBe('string');
+      expect(actual[0]).toBe('AAPL');
+      expect(actual[1]).toBe('AMZN');
+      expect(actual[2]).toBe('SHOP');
+    });
+  });
+});
