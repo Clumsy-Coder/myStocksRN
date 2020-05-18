cde30dadb1cd7e3bdffe7e6356fbf719d8ba195d
Clumsy-Coder <ubari94@gmail.com>
2020-05-17 19:36:26 -0600

    Add Action creators for Stock Chart

--- src/redux/Stocks/Actions.ts
+++ src/redux/Stocks/Actions.ts
@@ -1,3 +1,5 @@
+import { chartRange } from 'src/share/Constants';
+
 import {
   FetchStockQuoteAC,
   FetchStockChartAC,
@@ -6,6 +8,10 @@ import {
   FetchStockQuoteFulfilledAC,
   StockQuote,
   FetchStockQuoteRejectedAC,
+  FetchStockChartPendingAC,
+  FetchStockChartFulfilledAC,
+  StockChart,
+  FetchStockChartRejectedAC,
 } from 'src/redux/Stocks/Types';
 
 /**
@@ -63,7 +69,62 @@ export const fetchStockQuoteRejected = (
 
 // ---------------------------------------------------------------------------------------------- //
 
-export const fetchStockChart = (stockSymbol: string): FetchStockChartAC => ({
+/**
+ * Fetch stock chart action creator.
+ * ONLY to be called by the front end.
+ * @param stockSymbol - Company stock symbol in uppercase. Ex: AAPL
+ * @param range - Date range for the stock chart
+ */
+export const fetchStockChart = (
+  stockSymbol: string,
+  range: chartRange,
+  sort: 'asc' | 'desc',
+): FetchStockChartAC => ({
   type: ActionTypesEnum.FETCH_STOCK_CHART,
   stockSymbol,
+  range,
+  sort,
+});
+
+/**
+ * Fetch stock chart pending action creator.
+ * ONLY to be called by redux saga.
+ * Only used for internal use.
+ * @param stockSymbol - Company stock symbol in uppercase. Ex: AAPL
+ */
+export const fetchStockChartPending = (stockSymbol: string): FetchStockChartPendingAC => ({
+  type: ActionTypesEnum.FETCH_STOCK_CHART_PENDING,
+  stockSymbol,
+});
+
+/**
+ * Fetch Stock chart fulfilled action creator.
+ * ONLY to be called by redux saga.
+ * Only used for internal use.
+ * @param stockSymbol - Company stock symbol in uppercase. Ex: AAPL
+ * @param payload - data fetched
+ */
+export const fetchStockChartFulfilled = (
+  stockSymbol: string,
+  payload: StockChart[],
+): FetchStockChartFulfilledAC => ({
+  type: ActionTypesEnum.FETCH_STOCK_CHART_FULFILLED,
+  stockSymbol,
+  payload: { data: payload },
+});
+
+/**
+ * Fetch Stock chart rejected action creator.
+ * ONLY to be called by redux saga.
+ * Only used for internal use.
+ * @param stockSymbol - Company stock symbol in uppercase. Ex: AAPL
+ * @param error - error message
+ */
+export const fetchStockChartRejected = (
+  stockSymbol: string,
+  error: Error,
+): FetchStockChartRejectedAC => ({
+  type: ActionTypesEnum.FETCH_STOCK_CHART_REJECTED,
+  stockSymbol,
+  error,
 });
