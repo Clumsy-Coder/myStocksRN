8dbe6f5b5a18ebb91a1e232ee42a01bad808d220
Clumsy-Coder <ubari94@gmail.com>
2020-05-23 18:04:31 -0600

    Update changes from Stock redux Type definitions into index.reducers.ts
    [ci skip]

--- src/redux/index.reducers.ts
+++ src/redux/index.reducers.ts
@@ -2,11 +2,11 @@ import { combineReducers, Reducer } from 'redux';
 
 import StockReducer from 'src/redux/Stocks/Reducer';
 import FavoritesReducer from 'src/redux/Favorites/Reducer';
-import { StocksActions, StocksReducerState } from 'src/redux/Stocks/Types';
+import { StocksActions, Reducer as StockReducerType } from 'src/redux/Stocks/Types';
 import { FavoritesActions, FavoritesReducerState } from 'src/redux/Favorites/Types';
 
 export interface AppState {
-  Stocks: StocksReducerState;
+  Stocks: StockReducerType.ReducerState;
   Favorites: FavoritesReducerState;
 }
 export type AppActions = StocksActions | FavoritesActions;
