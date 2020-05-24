c4a2df7daa2b945b8b9e8caf679ec6bcd6667b5d
Clumsy-Coder <ubari94@gmail.com>
2020-05-23 18:12:12 -0600

    Fix Typescript and eslint issues for Stocks Sagas.test.ts

--- src/redux/Stocks/tests/Sagas.test.ts
+++ src/redux/Stocks/tests/Sagas.test.ts
@@ -5,65 +5,177 @@ import { takeEvery } from 'redux-saga/effects';
 import { expectSaga } from 'redux-saga-test-plan';
 import * as matchers from 'redux-saga-test-plan/matchers';
 
-import sagaWatcher, { fetchStockQuoteSaga, fetchStockChartSaga } from 'src/redux/Stocks/Sagas';
-import { ActionTypes, StockQuote, StockChart, StocksActions } from 'src/redux/Stocks/Types';
+import sagaWatcher, {
+  fetchStockQuoteSaga,
+  fetchStockDailyAdjustedSaga,
+} from 'src/redux/Stocks/Sagas';
+import { ActionTypes, StocksActions, DataDomain, Actions } from 'src/redux/Stocks/Types';
 import * as actions from 'src/redux/Stocks/Actions';
 import * as api from 'src/share/Utilities';
 
 const stockSymbol = 'AAPL';
-const data1: StockQuote = {
-  symbol: 'AAPL',
-  companyName: 'AAPL, Inc.',
-  primaryExchange: 'NghcwnSo k ktxeor eYacE',
-  open: 733,
-  close: 744.34,
-  high: 736,
-  low: 725,
-  latestPrice: 720.32,
-  latestTime: 'May 8, 2020',
-  latestUpdate: 1625938639425,
-  latestVolume: 4134691,
-  extendedPrice: 726.91,
-  extendedChange: 3.1,
-  extendedChangePercent: 0.4194815319493806,
-  previousClose: 730.16,
-  previousVolume: 4741435,
-  change: -13.97,
-  changePercent: -1.966,
-  avgTotalVolume: 4147824,
-  marketCap: 83884683022,
-  peRatio: -626.03,
-  week52High: 749.47,
-  week52Low: 247.61,
-  ytdChange: 75.35366771277485,
+const stockQuoteData1: DataDomain.StockQuote = {
+  'Global Quote': {
+    '01. symbol': 'IBM',
+    '02. open': '119.3700',
+    '03. high': '119.4650',
+    '04. low': '117.5900',
+    '05. price': '118.3900',
+    '06. volume': '4179906',
+    '07. latest trading day': '2020-05-22',
+    '08. previous close': '119.1200',
+    '09. change': '-0.7300',
+    '10. change percent': '-0.6128%',
+  },
 };
 
-const stockChartData1: StockChart[] = [
-  {
-    date: '2020-05-11',
-    open: 710.61,
-    close: 758.74,
-    high: 770.9,
-    low: 737.75,
-    volume: 3675742,
-    change: 0,
-    changePercent: 0,
-    label: 'May 11',
-    changeOverTime: 0,
+const stockQuoteData2: DataDomain.StockQuote = {
+  'Global Quote': {
+    '01. symbol': 'AAPL',
+    '02. open': '315.7700',
+    '03. high': '319.2300',
+    '04. low': '315.3500',
+    '05. price': '318.8900',
+    '06. volume': '20240356',
+    '07. latest trading day': '2020-05-22',
+    '08. previous close': '316.8500',
+    '09. change': '2.0400',
+    '10. change percent': '0.6438%',
   },
-  {
-    date: '2020-05-12',
-    open: 798,
-    close: 750.68,
-    high: 803.67,
-    low: 743.5,
-    volume: 3773050,
-    change: -8.41,
-    changePercent: -1.0796,
-    label: 'May 12',
-    changeOverTime: -0.010945,
+};
+
+const stockQuoteData3: DataDomain.StockQuote = {
+  'Global Quote': {
+    '01. symbol': 'SHOP',
+    '02. open': '804.6300',
+    '03. high': '826.3799',
+    '04. low': '793.6400',
+    '05. price': '825.1700',
+    '06. volume': '2761628',
+    '07. latest trading day': '2020-05-22',
+    '08. previous close': '802.3500',
+    '09. change': '22.8200',
+    '10. change percent': '2.8441%',
   },
-];
+};
+
+const stockDailyAdjData1: DataDomain.StockDailyAdj = {
+  'Meta Data': {
+    '1. Information': 'Daily Time Series with Splits and Dividend Events',
+    '2. Symbol': 'IBM',
+    '3. Last Refreshed': '2020-05-22',
+    '4. Output Size': 'Compact',
+    '5. Time Zone': 'US/Eastern',
+  },
+  'Time Series (Daily)': {
+    '2020-05-22': {
+      '1. open': '119.3700',
+      '2. high': '119.4650',
+      '3. low': '117.5900',
+      '4. close': '118.3900',
+      '5. adjusted close': '118.3900',
+      '6. volume': '4179906',
+      '7. dividend amount': '0.0000',
+      '8. split coefficient': '1.0000',
+    },
+    '2020-05-21': {
+      '1. open': '120.9900',
+      '2. high': '121.7200',
+      '3. low': '118.9700',
+      '4. close': '119.1200',
+      '5. adjusted close': '119.1200',
+      '6. volume': '4018329',
+      '7. dividend amount': '0.0000',
+      '8. split coefficient': '1.0000',
+    },
+  },
+};
+
+const stockDailyAdjData2: DataDomain.StockDailyAdj = {
+  'Meta Data': {
+    '1. Information': 'Daily Time Series with Splits and Dividend Events',
+    '2. Symbol': 'AAPL',
+    '3. Last Refreshed': '2020-05-22',
+    '4. Output Size': 'Compact',
+    '5. Time Zone': 'US/Eastern',
+  },
+  'Time Series (Daily)': {
+    '2020-05-22': {
+      '1. open': '315.7700',
+      '2. high': '319.2300',
+      '3. low': '315.3500',
+      '4. close': '318.8900',
+      '5. adjusted close': '318.8900',
+      '6. volume': '20240356',
+      '7. dividend amount': '0.0000',
+      '8. split coefficient': '1.0000',
+    },
+    '2020-05-21': {
+      '1. open': '318.6600',
+      '2. high': '320.8900',
+      '3. low': '315.8700',
+      '4. close': '316.8500',
+      '5. adjusted close': '316.8500',
+      '6. volume': '25672211',
+      '7. dividend amount': '0.0000',
+      '8. split coefficient': '1.0000',
+    },
+  },
+};
+
+const stockSearchData: DataDomain.StockSearch = {
+  bestMatches: [
+    {
+      '1. symbol': 'IBM',
+      '2. name': 'International Business Machines Corporation',
+      '3. type': 'Equity',
+      '4. region': 'United States',
+      '5. marketOpen': '09:30',
+      '6. marketClose': '16:00',
+      '7. timezone': 'UTC-05',
+      '8. currency': 'USD',
+      '9. matchScore': '1.0000',
+    },
+    {
+      '1. symbol': 'IBMM',
+      '2. name': 'iShares iBonds Dec 2024 Term Muni Bond ETF',
+      '3. type': 'ETF',
+      '4. region': 'United States',
+      '5. marketOpen': '09:30',
+      '6. marketClose': '16:00',
+      '7. timezone': 'UTC-05',
+      '8. currency': 'USD',
+      '9. matchScore': '0.8571',
+    },
+  ],
+};
+
+const stockSearchData2: DataDomain.StockSearch = {
+  bestMatches: [
+    {
+      '1. symbol': 'AAPL',
+      '2. name': 'Apple Inc.',
+      '3. type': 'Equity',
+      '4. region': 'United States',
+      '5. marketOpen': '09:30',
+      '6. marketClose': '16:00',
+      '7. timezone': 'UTC-05',
+      '8. currency': 'USD',
+      '9. matchScore': '1.0000',
+    },
+    {
+      '1. symbol': 'AAPL.ARG',
+      '2. name': 'Apple Inc.',
+      '3. type': 'Equity',
+      '4. region': 'Argentina',
+      '5. marketOpen': '11:00',
+      '6. marketClose': '17:00',
+      '7. timezone': 'UTC-03',
+      '8. currency': 'ARS',
+      '9. matchScore': '0.7273',
+    },
+  ],
+};
 
 describe('Stocks Saga', () => {
   describe('Stocks quote watcher', () => {
@@ -77,10 +189,12 @@ describe('Stocks Saga', () => {
 
     it(`Should fire on ${ActionTypes.FETCH_STOCK_QUOTE} action in case success`, () => {
       return expectSaga(sagaWatcher)
-        .provide([[matchers.call.fn(api.fetchStockQuoteUrl), Promise.resolve({ data: data1 })]])
+        .provide([
+          [matchers.call.fn(api.fetchStockQuoteUrl), Promise.resolve({ data: stockQuoteData1 })],
+        ])
         .put(actions.fetchStockQuotePending(stockSymbol))
         .call(api.fetchStockQuoteUrl, stockSymbol)
-        .put(actions.fetchStockQuoteFulfilled(stockSymbol, data1))
+        .put(actions.fetchStockQuoteFulfilled(stockSymbol, stockQuoteData1))
         .not.put(actions.fetchStockQuoteRejected(stockSymbol, new Error('')))
         .dispatch(actions.fetchStockQuote(stockSymbol))
         .silentRun()
@@ -93,7 +207,7 @@ describe('Stocks Saga', () => {
         .put(actions.fetchStockQuotePending(stockSymbol))
         .call(api.fetchStockQuoteUrl, stockSymbol)
         .put(actions.fetchStockQuoteRejected(stockSymbol, new Error('')))
-        .not.put(actions.fetchStockQuoteFulfilled(stockSymbol, data1))
+        .not.put(actions.fetchStockQuoteFulfilled(stockSymbol, stockQuoteData1))
         .dispatch(actions.fetchStockQuote(stockSymbol))
         .silentRun()
         .then((result) => expect(result.toJSON()).toMatchSnapshot());
@@ -107,8 +221,8 @@ describe('Stocks Saga', () => {
         getState: (): {} => ({}),
         dispatch: (action: StocksActions): number => dispatchedActions.push(action),
       };
-      const promiseResponse: AxiosResponse<StockQuote> = {
-        data: data1,
+      const promiseResponse: AxiosResponse<DataDomain.StockQuote> = {
+        data: stockQuoteData1,
         headers: {},
         status: 200,
         statusText: '',
@@ -122,7 +236,9 @@ describe('Stocks Saga', () => {
       expect(stub.calledOnce).toBe(true);
       expect(dispatchedActions.length).toEqual(2);
       expect(dispatchedActions[0]).toEqual(actions.fetchStockQuotePending(stockSymbol));
-      expect(dispatchedActions[1]).toEqual(actions.fetchStockQuoteFulfilled(stockSymbol, data1));
+      expect(dispatchedActions[1]).toEqual(
+        actions.fetchStockQuoteFulfilled(stockSymbol, stockQuoteData1),
+      );
 
       stub.restore(); // important: do NOT remove
     });
@@ -155,8 +271,8 @@ describe('Stocks Saga', () => {
         getState: (): {} => ({}),
         dispatch: (action: StocksActions): number => dispatchedActions.push(action),
       };
-      const promiseResponse: AxiosResponse<StockChart[]> = {
-        data: stockChartData1,
+      const promiseResponse: AxiosResponse<DataDomain.StockDailyAdj> = {
+        data: stockDailyAdjData1,
         headers: {},
         status: 200,
         statusText: '',
@@ -164,18 +280,20 @@ describe('Stocks Saga', () => {
         config: {},
       };
 
-      const stub = sinon.stub(api, 'fetchStockChartUrl').returns(Promise.resolve(promiseResponse));
+      const stub = sinon
+        .stub(api, 'fetchStockDailyAdjustedUrl')
+        .returns(Promise.resolve(promiseResponse));
       await runSaga(
         fakeStore,
-        fetchStockChartSaga,
-        actions.fetchStockChart(stockSymbol, '5d', 'asc'),
+        fetchStockDailyAdjustedSaga,
+        actions.fetchStockDailyAdj(stockSymbol, '5d', 'asc'),
       );
 
       expect(stub.calledOnce).toBe(true);
       expect(dispatchedActions.length).toEqual(2);
-      expect(dispatchedActions[0]).toEqual(actions.fetchStockChartPending(stockSymbol));
+      expect(dispatchedActions[0]).toEqual(actions.fetchStockDailyAdjPending(stockSymbol));
       expect(dispatchedActions[1]).toEqual(
-        actions.fetchStockChartFulfilled(stockSymbol, stockChartData1),
+        actions.fetchStockDailyAdjFulfilled(stockSymbol, stockDailyAdjData1),
       );
 
       stub.restore(); // important: do NOT remove
@@ -188,18 +306,20 @@ describe('Stocks Saga', () => {
         dispatch: (action: StocksActions): number => dispatchedActions.push(action),
       };
 
-      const stub = sinon.stub(api, 'fetchStockChartUrl').returns(Promise.reject(new Error('')));
+      const stub = sinon
+        .stub(api, 'fetchStockDailyAdjustedUrl')
+        .returns(Promise.reject(new Error('')));
       await runSaga(
         fakeStore,
-        fetchStockChartSaga,
-        actions.fetchStockChart(stockSymbol, '5d', 'asc'),
+        fetchStockDailyAdjustedSaga,
+        actions.fetchStockDailyAdj(stockSymbol, '5d', 'asc'),
       );
 
       expect(stub.calledOnce).toBe(true);
       expect(dispatchedActions.length).toEqual(2);
-      expect(dispatchedActions[0]).toEqual(actions.fetchStockChartPending(stockSymbol));
+      expect(dispatchedActions[0]).toEqual(actions.fetchStockDailyAdjPending(stockSymbol));
       expect(dispatchedActions[1]).toEqual(
-        actions.fetchStockChartRejected(stockSymbol, new Error('')),
+        actions.fetchStockDailyAdjRejected(stockSymbol, new Error('')),
       );
 
       stub.restore(); // important: do NOT remove
