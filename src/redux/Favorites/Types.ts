3a4aa19879fb2c70379e5f9ecca73dd2e424971b
Clumsy-Coder <ubari94@gmail.com>
2020-05-26 12:30:30 -0600

    Add FavoriteStockData and wrap actions and reducer interface into a namespace
    The FavoriteStockData will be used for saving favorites stock instead of
    a string of the stock symbol

--- src/redux/Favorites/Types.ts
+++ src/redux/Favorites/Types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import { Action } from 'redux';
 
 /*
@@ -10,11 +11,25 @@ import { Action } from 'redux';
 
 */
 
-/**
- * Favorites reducer state
- */
-export interface FavoritesReducerState {
-  symbols: string[];
+export namespace Reducer {
+  export interface FavoriteStockData {
+    '1. symbol': string;
+    '2. name': string;
+    // '3. type': string;
+    '4. region': string;
+    // '5. marketOpen': string;
+    // '6. marketClose': string;
+    '7. timezone': string;
+    '8. currency': string;
+    // '9. matchScore': string;
+  }
+
+  /**
+   * Favorites reducer state
+   */
+  export interface FavoritesReducerState {
+    symbols: FavoriteStockData[];
+  }
 }
 
 // ---------------------------------------------------------------------------------------------- //
@@ -48,33 +63,35 @@ export enum ActionTypes {
 
 */
 
-/**
- * Add stock to Favorites action
- */
-export interface AddFavoriteStockAction extends Action<ActionTypes.ADD_FAVORITE_STOCK> {
-  type: ActionTypes.ADD_FAVORITE_STOCK;
-  readonly stockSymbol: string;
-}
+export namespace Actions {
+  /**
+   * Add stock to Favorites action
+   */
+  export interface AddFavoriteStockAction extends Action<ActionTypes.ADD_FAVORITE_STOCK> {
+    type: ActionTypes.ADD_FAVORITE_STOCK;
+    readonly stockMetadata: Reducer.FavoriteStockData;
+  }
 
-/**
- * Remove stock from Favorites action
- */
-export interface RemoveFavoriteStockAction extends Action<ActionTypes.REMOVE_FAVORITE_STOCK> {
-  type: ActionTypes.REMOVE_FAVORITE_STOCK;
-  readonly stockSymbol: string;
-}
+  /**
+   * Remove stock from Favorites action
+   */
+  export interface RemoveFavoriteStockAction extends Action<ActionTypes.REMOVE_FAVORITE_STOCK> {
+    type: ActionTypes.REMOVE_FAVORITE_STOCK;
+    readonly stockSymbol: string;
+  }
 
-/**
- * Clear all stocks from Favorites action
- */
-export interface ClearFavoriteStocksAction extends Action<ActionTypes.CLEAR_FAVORITE_STOCKS> {
-  type: ActionTypes.CLEAR_FAVORITE_STOCKS;
+  /**
+   * Clear all stocks from Favorites action
+   */
+  export interface ClearFavoriteStocksAction extends Action<ActionTypes.CLEAR_FAVORITE_STOCKS> {
+    type: ActionTypes.CLEAR_FAVORITE_STOCKS;
+  }
 }
 
 /**
  * Union of actions for AddFavoriteStock, RemoveFavoriteStock, ClearFavoriteStocks
  */
 export type FavoritesActions =
-  | AddFavoriteStockAction
-  | RemoveFavoriteStockAction
-  | ClearFavoriteStocksAction;
+  | Actions.AddFavoriteStockAction
+  | Actions.RemoveFavoriteStockAction
+  | Actions.ClearFavoriteStocksAction;
