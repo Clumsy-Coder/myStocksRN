0e88404f28e39e641b685c36e9c14b5877525cae
Clumsy-Coder <ubari94@gmail.com>
2020-06-06 17:47:06 -0600

    Render stock quote trim on Home screen
    - Uses selector selectStockQuoteTrim to process the data.
    - Renders the list using StockCard functional component.
    - Fetches the stock quote data after component mountss

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,34 +1,64 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Container, Content, List } from 'native-base';
 
-import NavigationRoutePath from 'src/routes/RoutePath.enum';
+import { AppActions, AppState } from 'src/redux/index.reducers';
 import { RootStackParamList } from 'src/routes/AppRouteTypes';
+import { fetchStockQuoteBatch } from 'src/redux/Stocks/Actions';
+import { Actions as StocksActions, Selectors as StocksSelectors } from 'src/redux/Stocks/Types';
+import { selectStockQuoteTrim } from 'src/redux/Stocks/Selectors';
+import StockCard from 'src/containers/StockCard';
 
-type Props = {
+interface OwnProps {
   navigation: StackNavigationProp<RootStackParamList, 'Home'>;
-};
+}
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+interface SelectorProps {
+  selectedStockQuoteTrim: StocksSelectors.SelectQuoteTrim[];
+}
+
+interface DispatchProps {
+  fetchQuoteBatch: () => StocksActions.Batch.FetchQuoteAction;
+}
+
+type Props = SelectorProps & DispatchProps & OwnProps;
+
+export class Home extends React.Component<Props> {
+  componentDidMount(): void {
+    const { fetchQuoteBatch } = this.props;
+    fetchQuoteBatch();
+  }
+
+  render(): JSX.Element {
+    const { selectedStockQuoteTrim } = this.props;
+
+    return (
+      <Container>
+        <Content>
+          <List>
+            {selectedStockQuoteTrim.map((stockQuoteTrim: StocksSelectors.SelectQuoteTrim) => {
+              return (
+                <StockCard
+                  data={stockQuoteTrim}
+                  key={`stock-quote-trim-${stockQuoteTrim.symbol}`}
+                />
+              );
+            })}
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = (state: AppState): SelectorProps => ({
+  selectedStockQuoteTrim: selectStockQuoteTrim(state),
 });
 
-const Home: React.FC<Props> = ({ navigation }: Props) => (
-  <View style={styles.root}>
-    <Text>Home screen</Text>
-    <Button
-      onPress={(): void => navigation.navigate(NavigationRoutePath.StockDetails)}
-      title='Go to StockDetails'
-    />
-    <Button
-      onPress={(): void => navigation.navigate(NavigationRoutePath.About)}
-      title='Go to About'
-    />
-  </View>
-);
+const mapDispatchToProps = (dispatch: Dispatch<AppActions>): DispatchProps => ({
+  fetchQuoteBatch: (): StocksActions.Batch.FetchQuoteAction => dispatch(fetchStockQuoteBatch()),
+});
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
