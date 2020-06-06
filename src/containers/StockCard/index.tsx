7005c4fe2ec7283d37769fe54969120e47beee83
Clumsy-Coder <ubari94@gmail.com>
2020-06-06 17:39:56 -0600

    Add StockCard functional component
    Displays one stock symbol basic info.
    - Stock symbol
    - Company name
    - Stock price
    - Stock price change

---
+++ src/containers/StockCard/index.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { ListItem, Text, H1, H3 } from 'native-base';
+
+import { Selectors } from 'src/redux/Stocks/Types';
+
+interface OwnProps {
+  data: Selectors.SelectQuoteTrim;
+}
+
+const styleSheet = StyleSheet.create({
+  // main container
+  container: {
+    // backgroundColor: 'blue',
+  },
+  listItemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 70,
+  },
+  // company
+  companyContainer: {
+    flex: 3,
+    flexDirection: 'column',
+    // backgroundColor: 'orange',
+    justifyContent: 'space-around',
+  },
+  companyStockSymbol: {
+    fontWeight: 'bold',
+  },
+  companySymbol: {
+    fontWeight: 'bold',
+  },
+  companyName: {},
+  // price
+  priceContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    // backgroundColor: 'pink',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+  },
+  priceValue: {},
+  priceChangeContainer: {
+    borderRadius: 4,
+    padding: 1,
+    paddingVertical: 2,
+    paddingRight: 5,
+    width: '100%',
+    alignItems: 'flex-end',
+  },
+  priceChange: {
+    color: 'white',
+  },
+  priceRed: {
+    backgroundColor: 'red',
+  },
+  priceGreen: {
+    backgroundColor: 'green',
+  },
+});
+
+const StockCard: React.FC<OwnProps> = (props: OwnProps) => {
+  const { data } = props;
+  if (data.fetching) {
+    return (
+      <ListItem>
+        <Text>Loading</Text>
+      </ListItem>
+    );
+  }
+
+  const change = data.change === undefined ? '0' : data.change;
+
+  return (
+    <ListItem style={styleSheet.container}>
+      <View style={styleSheet.listItemContainer}>
+        <View style={styleSheet.companyContainer}>
+          <H1 style={styleSheet.companySymbol}>{data.symbol}</H1>
+          <H3 style={styleSheet.companyName}>{data.companyName}</H3>
+        </View>
+        <View style={styleSheet.priceContainer}>
+          <H3>{data.price}</H3>
+          <View
+            style={[
+              styleSheet.priceChangeContainer,
+              +change > 0 ? styleSheet.priceGreen : styleSheet.priceRed,
+            ]}
+          >
+            <H3 style={styleSheet.priceChange}>{data.change}</H3>
+          </View>
+        </View>
+      </View>
+    </ListItem>
+  );
+};
+
+export default StockCard;
