f0fdc50456fa83c93a7a46f38ed1a9d359b0e2f7
Clumsy-Coder <ubari94@gmail.com>
2020-06-12 12:30:06 -0600

    Remove React Navigation from OwnProps interface
    It was intended to be used for navigating to another screen when a stock
    card was pressed.
    
    That will be done in StockCard component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import { StackNavigationProp } from '@react-navigation/stack';
 import { Container, Content, List } from 'native-base';
 
 import { AppActions, AppState } from 'src/redux/index.reducers';
-import { RootStackParamList } from 'src/routes/AppRouteTypes';
 import { fetchStockQuoteBatch } from 'src/redux/Stocks/Actions';
 import { Actions as StocksActions, Selectors as StocksSelectors } from 'src/redux/Stocks/Types';
 import { selectStockQuoteTrim } from 'src/redux/Stocks/Selectors';
 import StockCard from 'src/components/StockCard';
-
-interface OwnProps {
-  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
-}
 
 interface SelectorProps {
   selectedStockQuoteTrim: StocksSelectors.SelectQuoteTrim[];
@@ -23,7 +17,7 @@ interface DispatchProps {
   fetchQuoteBatch: () => StocksActions.Batch.FetchQuoteAction;
 }
 
-type Props = SelectorProps & DispatchProps & OwnProps;
+type Props = SelectorProps & DispatchProps;
 
 export class Home extends React.Component<Props> {
   componentDidMount(): void {
