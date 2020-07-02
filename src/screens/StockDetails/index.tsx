cc4d08acd3ce51ae6ca2efc43370cb7ab13a2347
Clumsy-Coder <ubari94@gmail.com>
2020-07-02 10:27:22 -0600

    Update StockDetails screen
    - Change functional component to Component
    - Add mapStateToProps typedef
    - Add mapDispatchToProps typedef
    - Add NavigationProps typedef
    - Fetch the data using Stocks selector 'selectStockDetailsTrim'
    - Map redux dispatch to fetch Stock Charts data
    - Connect component with react redux
    - Fetch stocks chart data after component has mounted
    - Display the keystats using 'StocksKeyStats' component

--- src/screens/StockDetails/index.tsx
+++ src/screens/StockDetails/index.tsx
@@ -1,42 +1,86 @@
 import * as React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { Container, Content, Text, H1, H2, H3, View } from 'native-base';
+import { Dispatch } from 'redux';
 
-import { NavigationRoutePath, RootStackParamList, StockDetailsRouteProp } from '@routes/Types';
-import { useRoute } from '@react-navigation/native';
+import { StockDetailsRouteProp } from '@routes/Types';
+import { AppState, AppActions } from '@redux/index.reducers';
+import { Actions, Selectors } from '@redux/Stocks/Types';
+import { fetchStockChart } from '@redux/Stocks/Actions';
+import { selectStockDetailsTrim } from '@redux/Stocks/Selectors';
+import StocksKeyStats from '@components/StocksKeyStats';
 
-type Props = {
-  navigation: StackNavigationProp<RootStackParamList, NavigationRoutePath.StockDetails>;
+interface SelectorProps {
+  data: Selectors.SelectStockDetailsTrim;
+}
+
+interface DispatchProps {
+  fetchChart: (symbol: string) => Actions.Chart.FetchAction;
+}
+
+type NavigationProps = {
+  route: StockDetailsRouteProp;
 };
+
+type Props = NavigationProps & SelectorProps & DispatchProps;
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 10,
+  },
+  price: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    paddingTop: 20,
   },
 });
 
-const StockDetailScreen: React.FC<Props> = ({ navigation }: Props) => {
-  const route = useRoute<StockDetailsRouteProp>();
-  const { symbol } = route.params;
+class StockDetailsScreen extends React.Component<Props> {
+  componentDidMount(): void {
+    const { route, fetchChart } = this.props;
 
-  return (
-    <View style={styles.root}>
-      <Text>Stock details screen</Text>
-      <Button
-        onPress={(): void => navigation.navigate(NavigationRoutePath.Home)}
-        title='Go to Home screen'
-      />
-      <Text>{'\n'}</Text>
-      <Button
-        onPress={(): void => navigation.navigate(NavigationRoutePath.About)}
-        title='Go to About screen'
-      />
-      <Text>{'\n'}</Text>
-      <Text>Symbol: {symbol}</Text>
-    </View>
-  );
-};
+    fetchChart(route.params.symbol);
+  }
 
-export default StockDetailScreen;
+  render(): JSX.Element {
+    const { route, data } = this.props;
+    const { symbol } = route.params;
+
+    if (data.fetching) {
+      return (
+        <Container>
+          <Content>
+            <Text>Loading</Text>
+          </Content>
+        </Container>
+      );
+    }
+
+    return (
+      <Container style={styles.root}>
+        <Content>
+          <View>
+            <H1>{data.quote?.companyName}</H1>
+          </View>
+          <H1 style={styles.price}>{data.quote.latestPrice}</H1>
+          <StocksKeyStats data={data.quote} />
+        </Content>
+      </Container>
+    );
+  }
+}
+
+export const mapStateToProps = (state: AppState, ownProps: Props): SelectorProps => ({
+  data: selectStockDetailsTrim(state, {
+    stockSymbol: ownProps.route.params.symbol,
+  }),
+});
+
+export const mapDispatchToProps = (dispatch: Dispatch<AppActions>): DispatchProps => ({
+  fetchChart: (symbol: string): Actions.Chart.FetchAction => dispatch(fetchStockChart(symbol)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StockDetailsScreen);
