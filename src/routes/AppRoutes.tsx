a2e713c58e1187612a6d106e90adbd1d6256beff
Clumsy-Coder <ubari94@gmail.com>
2020-07-20 17:35:01 -0600

    Connect Component to redux
    selector props:
    - get favorite stock symbols
    
    dispatch props:
    - add stock symbol to favorites
    - remove stock symbol from favorites

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 
 import HomeScreen from 'src/screens/Home';
 import StockDetailsScreen from 'src/screens/StockDetails';
 import AboutScreen from 'src/screens/About';
 import { NavigationRoutePath, RootStackParamList } from '@routes/Types';
+import { selectFavoriteSymbols } from '@redux/Favorites/Selectors';
+import { Actions } from '@redux/Favorites/Types';
+import { addFavoriteStock, removeFavoriteStock } from '@redux/Favorites/Actions';
+import { AppState, AppActions } from '@redux/index.reducers';
+
+interface SelectorProps {
+  favoriteSymbols: string[];
+}
+
+interface DispatchProps {
+  addToFavorites: (symbol: string) => Actions.AddFavoriteStockAction;
+  removeFromFavorites: (symbol: string) => Actions.RemoveFavoriteStockAction;
+}
+
+type OwnProps = SelectorProps & DispatchProps;
 
 const Stack = createStackNavigator<RootStackParamList>();
-
-const HomeStackNavigator: React.FC = () => (
+const HomeStackNavigator: React.FC<OwnProps> = () => (
   <Stack.Navigator
     initialRouteName={NavigationRoutePath.Home}
     screenOptions={{
@@ -25,4 +41,15 @@ const HomeStackNavigator: React.FC = () => (
   </Stack.Navigator>
 );
 
-export default HomeStackNavigator;
+const mapStateToProps = (state: AppState): SelectorProps => ({
+  favoriteSymbols: selectFavoriteSymbols(state),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<AppActions>): DispatchProps => ({
+  addToFavorites: (symbol: string): Actions.AddFavoriteStockAction =>
+    dispatch(addFavoriteStock(symbol)),
+  removeFromFavorites: (symbol: string): Actions.RemoveFavoriteStockAction =>
+    dispatch(removeFavoriteStock(symbol)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeStackNavigator);
