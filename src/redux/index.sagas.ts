8737de43e088a0043635ec6bb099aa164bfa96d2
Clumsy-Coder <ubari94@gmail.com>
2020-08-02 18:45:18 -0600

    fix(saga): fix eslint error 'A Saga must handle its effects' errors (use try/catch)'

--- src/redux/index.sagas.ts
+++ src/redux/index.sagas.ts
@@ -5,5 +5,7 @@ import stocksSagaWatcher from 'src/redux/Stocks/Sagas';
 type RootSaga = Generator<AllEffect<Generator<ForkEffect<never>, void, unknown>>, void, unknown>;
 
 export default function* (): RootSaga {
-  yield all([stocksSagaWatcher()]);
+  try {
+    yield all([stocksSagaWatcher()]);
+  } catch (error) {}
 }
