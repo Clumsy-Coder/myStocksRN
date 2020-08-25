885c68c2dad64aa43c7e4824ec31dd7bde6869d7
Clumsy-Coder <ubari94@gmail.com>
2020-08-25 12:32:17 -0600

    ci(semantic-release): add commitTypes file
    This will be used when generating changelog for each commit type.
    
    NOTE: need to replace gitmoji markup with unicode gitmoji

---
+++ semantic-release/commitTypes.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    type: 'feat',
+    section: ':sparkles: Features',
+  },
+  {
+    type: 'feature',
+    section: ':sparkles: Features',
+  },
+  {
+    type: 'fix',
+    section: ':bug: Bug Fixes',
+  },
+  {
+    type: 'perf',
+    section: ':zap: Performance Improvements',
+  },
+  {
+    type: 'revert',
+    section: ':rewind: Reverts',
+  },
+  {
+    type: 'docs',
+    section: ':pencil: Documentation',
+  },
+  {
+    type: 'style',
+    section: ':lipstick: Styles',
+  },
+  {
+    type: 'chore',
+    section: ':robot: Miscellaneous Chores',
+  },
+  {
+    type: 'refactor',
+    section: ':recycle: Code Refactoring',
+  },
+  {
+    type: 'test',
+    section: ':white_check_mark: Tests',
+  },
+  {
+    type: 'build',
+    section: ':package: Build System',
+  },
+  {
+    type: 'ci',
+    section: ':construction_worker: Continuous Integration',
+  },
+];
