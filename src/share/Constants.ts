52b134db6d12faf23a6d05297cd4338fb0f7677a
Clumsy-Coder <ubari94@gmail.com>
2020-09-07 08:56:47 -0600

    feat(env): add DEVELOPMENT mode when fetching data from API
    Add support to use Development API to fetch data. This will use a different API_KEY and API_URL.
    This will only be used for development. Shouldn't change anything when bulding production Android
    APK

--- src/share/Constants.ts
+++ src/share/Constants.ts
@@ -1,2 +1,5 @@
-export const PROD_API_URL = 'https://cloud.iexapis.com/stable';
-export const API_URL = PROD_API_URL;
+import { BUILD_VERSION } from 'react-native-dotenv';
+
+export const PROD_API_URL = 'https://cloud.iexapis.com/stable/';
+export const DEV_API_URL = 'https://sandbox.iexapis.com/stable/';
+export const API_URL = BUILD_VERSION === 'development' ? DEV_API_URL : PROD_API_URL;
